923c92c7709f173358722a1ad6dda35f89d7fbdd
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-01-27 13:28:47 +0530

    Add files via upload

---
+++ jan 25/ass1.js
@@ -0,0 +1,18 @@
+var n = 36;
+
+if (n==10)
+{
+console.log("number is equal to 10");
+}
+
+else if (n>10)
+{
+console.log("number is greater than 10")
+}
+
+else
+{
+console.log("number is less than 10")
+}
+
+
