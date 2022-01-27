6ebd18ddad528d00d6183821ca9fab18a52036d1
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-01-27 19:51:49 +0530

    Add files via upload

---
+++ jan 26/min.js
@@ -0,0 +1,19 @@
+var n1 = prompt("Enter first number");
+var n2 = prompt("Enter second number");
+var n3 = prompt("Enter third number");
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+n3 = parseInt(n3);
+
+if(n1<=n2 && n1<=n3)
+{
+alert(n1+ "is minimum");
+}
+else if(n2<=n1 && n2<=n3)
+{
+alert(n2+ "is minimum");
+}
+else
+{
+alert(n3+ "is minimum");
