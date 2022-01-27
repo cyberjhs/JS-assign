984e7f7d19ee5cea7320ac5f06797fc03071ba77
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-01-27 20:09:39 +0530

    Add files via upload

---
+++ jan 26/char.js
@@ -0,0 +1,10 @@
+var char = prompt("Enter any alphabet")
+
+if (char == 'a' || char =='e' || char == 'i' || char == 'o' ||char == 'u')
+{
+alert("char is vowel");
+}
+else
+{
+alert("char is consonant");
+}
