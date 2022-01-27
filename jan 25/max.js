1e9f9c8368d4b15a58cf5071cf5e0854f68ff1d6
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-01-27 13:28:07 +0530

    Add files via upload

---
+++ jan 25/max.js
@@ -0,0 +1,16 @@
+var p=20
+var q=21
+var r=5
+
+if(p>=q && p>=r)
+{
+cosole.log("maximum number is ",p)
+}
+else if(q>=p && q>=r)
+{
+console.log("maximum number is ",q)
+}
+else
+{
+console.log("maximim number is ",r)
+}
